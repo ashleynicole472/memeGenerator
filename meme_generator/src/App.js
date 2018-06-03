b4df192d5be6548ce18af6926a3de8b0baded51c
ashleynicole472 <asha.4720@gmail.com>
2018-06-03 14:45:16 -0700

    Added two text input fields, and a drop down menu with a button for users to create their meme in the front end.

--- meme_generator/src/App.js
+++ meme_generator/src/App.js
@@ -3,17 +3,88 @@ import './App.css'
 import AUX from './hoc/AUX';
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            topText: '',
+            bottomText: ''
+        }
+        this.topTextHandleChange = this.topTextHandleChange.bind(this);
+        this.bottomTextHandleChange = this.bottomTextHandleChange.bind(this);
+    }
+
+    topTextHandleChange = (event) => {
+        // console.log("In top text");
+        // console.log(this.state.topText);
+        this.setState({
+            topText: event.target.value
+        })
+    };
+
+    bottomTextHandleChange = (event) => {
+        // console.log("In bottom text");
+        // console.log(this.state.bottomText);
+        this.setState({
+            bottomText: event.target.value
+        })
+    };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <AUX>
+            <h1>Genorate your own meme!</h1>
+            <h5>How it works:</h5>
+            <ol>
+              <li>Type something for the top of the meme</li>
+              <li>Pick an image from the drop down menu</li>
+              <li>Type something for the bottom of the meme</li>
+            </ol>
+            <div>
+                <div className="input-group input-group-lg">
+                    <div className="input-group-prepend">
+                        <span className="input-group-text" id="inputGroup-sizing-lg">Top Text</span>
+                    </div>
+                    <input
+                        type="text"
+                        onChange={this.topTextHandleChange}
+                        className="form-control"
+                        aria-label="Large"
+                        aria-describedby="inputGroup-sizing-sm"
+                        value={this.state.topText}/>
+                </div>
+            </div>
+            <br />
+            <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                    <label className="input-group-text" for="inputGroupSelect01">Pick Image</label>
+                </div>
+                <select
+                    className="custom-select"
+                    id="inputGroupSelect01">
+                    <option selected>Choose...</option>
+                    <option value="1">one</option>
+                    <option value="2">two</option>
+                </select>
+            </div>
+            <div>
+                <div className="input-group input-group-lg">
+                    <div className="input-group-prepend">
+                        <span className="input-group-text" id="inputGroup-sizing-lg">Bottom Text</span>
+                    </div>
+                    <input
+                        type="text"
+                        onChange={this.bottomTextHandleChange}
+                        className="form-control"
+                        aria-label="Large"
+                        aria-describedby="inputGroup-sizing-sm"
+                        value={this.state.bottomText}/>
+                </div>
+                <br />
+                <button
+                    type="button"
+                    className="btn btn-secondary btn-lg btn-block">Genorate Your Meme!!</button>
+            </div>
+        </AUX>
     );
   }
 }
